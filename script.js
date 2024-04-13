e6a39a245b23de6176be623138960641a67a46bb
Bikey Bhujel <bicky.bhujel@gmail.com>
2024-04-13 16:45:01 -0400

    taking values from rows i.e. t1 and columns i.e. t2 and making sure valid numbers are entered

--- script.js
+++ script.js
@@ -6,34 +6,5 @@ function generateTable() {
         alert("Please enter valid numbers for rows and columns.");
         return;
     }
-    var tableHTML = "<table border='1'>";
-    for (var i = 0; i < rows; i++) {
-        tableHTML += "<tr>";
-        for (var j = 0; j < cols; j++) {
-            tableHTML += "<td>Cell</td>";
-        }
-        tableHTML += "</tr>";
-    }
-    tableHTML += "</table>";
-    document.body.innerHTML = "<button id='b2' onclick='addColumn()'>Add Column</button><button id='b3' onclick='addRow()'>Add Row</button>" + tableHTML;
-}
-
-// Function to add a column to the table
-function addColumn() {
-    var rows = document.querySelectorAll("table tr");
-    for (var i = 0; i < rows.length; i++) {
-        rows[i].innerHTML += "<td>Cell</td>";
-    }
-}
-
-// Function to add a row to the table
-function addRow() {
-    var table = document.querySelector("table");
-    var cols = table.rows[0].cells.length;
-    var newRow = "<tr>";
-    for (var i = 0; i < cols; i++) {
-        newRow += "<td>Cell</td>";
-    }
-    newRow += "</tr>";
-    table.innerHTML += newRow;
+    
 }
